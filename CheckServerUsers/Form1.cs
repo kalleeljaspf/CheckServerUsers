9c8818b0c548d41e93079d9ac9fa12a962776ed5
Kalle Eljas <kalle.eljas@preciofishbone.se>
2019-09-11 14:18:43 +0200

    first commit

---
+++ CheckServerUsers/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CheckServerUsers
+{
+    public partial class Form1 : Form
+    {
+        private static StringBuilder cmdOutput = null;
+        Process cmdProcess;
+        StreamWriter cmdStreamWriter;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmdOutput = new StringBuilder("");
+            cmdProcess = new Process();
+
+            cmdProcess.StartInfo.FileName = "cmd.exe";
+            cmdProcess.StartInfo.UseShellExecute = false;
+            cmdProcess.StartInfo.CreateNoWindow = true;
+            cmdProcess.StartInfo.RedirectStandardOutput = true;
+
+            cmdProcess.OutputDataReceived += new DataReceivedEventHandler(SortOutputHandler);
+            cmdProcess.StartInfo.RedirectStandardInput = true;
+            cmdProcess.Start();
+
+            cmdStreamWriter = cmdProcess.StandardInput;
+            cmdProcess.BeginOutputReadLine();
+        }
+
+        private void SortOutputHandler(object sendingProcess, DataReceivedEventArgs outLane)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
